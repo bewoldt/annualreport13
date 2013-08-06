531acbd87b2084928d1a048588942c06cf2ce0d3
Brandon Ewoldt <bewoldt1@gmail.com>
2013-08-06 17:04:08 -0500

    new video site stuff

---
+++ home.php
@@ -0,0 +1,222 @@
+<?php include 'inc/doctype.php'; ?>
+<?php include 'inc/meta.php'; ?>
+<title>Home - Annual Report 2013</title>
+<!-- <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700, 800' rel='stylesheet' type='text/css'> -->
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,400italic' rel='stylesheet' type='text/css'>
+
+<!--[if lt IE 9]>
+    <script src="js/vendor/html5shiv.js"></script>
+    <script src="js/vendor/respond.min.js"></script>
+<![endif]-->
+<link rel="stylesheet" href="css/master.css">
+<link rel="stylesheet" href="css/home.css">
+</head>
+
+<body>
+	<div class="menu">
+
+	</div>
+	<div class="#container">
+		<div id="two-column">
+			<div class="mod-text">
+				<div class="mod-center">
+					<h1>Annual Report</h1>
+				</div>
+				
+			</div>
+			<div class="module_list">
+				<ul class="unstyled">
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Awarding Commitment</h3>
+							<span class="dept">Counseling Service</span>
+							<p class="excerpt">Attending college is not only about excelling academically, but also using those skills outside of the classroom.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Spirit and Sportsmanship</h3>
+							<span class="dept">Disibility Services</span>
+							<p class="excerpt">In the 2012-2013 academic year, Hawks Nest unveiled brand new membership benefits for students.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Leadership Intro Course</h3>
+							<span class="dept">Center for Student Involvement and Leadership</span>
+							<p class="excerpt">Students at the UI can learn what it means to be in a leadership position and develop a personal leadership style in the academic leadership classes.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Promoting Diversity and Inclusion on Campus</h3>
+							<span class="dept">Student Health and Wellness</span>
+							<p class="excerpt">“The Mirage” raises awareness for World Aids Day, provides free HIV testing during the event, and promotes diversity and inclusion on campus.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Welcoming 50 New Student Organizations</h3>
+							<span class="dept">Center for Student Involvement and Leadership</span>
+							<p class="excerpt">The Center for Student Involvement added over 50 student organizations during the 2012-2013 school year.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Beach Body Boot Camp</h3>
+							<span class="dept">Recreational Services</span>
+							<p class="excerpt">Beach Body Boot Camp is a 45-minute workout based on circuit training for UI community members.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Mollis Commodo Cursus Egestas</h3>
+							<span class="dept">Vestibulum Vulputate Pharetra</span>
+							<p class="excerpt">Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Vulputate Tortor Nibh Nullam</h3>
+							<span class="dept">Ipsum Consectetur</span>
+							<p class="excerpt">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Etiam Porta Consectetur</h3>
+							<span class="dept">Egestas Dapibus Venenatis</span>
+							<p class="excerpt">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Mollis Ornare Tellus</h3>
+							<span class="dept">Vehicula Pharetra</span>
+							<p class="excerpt">Vestibulum id ligula porta felis euismod semper.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Quam Ultricies Mattis</h3>
+							<span class="dept">Venenatis Condimentum Sollicitudin</span>
+							<p class="excerpt">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Quam Ultricies Mattis</h3>
+							<span class="dept">Venenatis Condimentum Sollicitudin</span>
+							<p class="excerpt">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Quam Ultricies Mattis</h3>
+							<span class="dept">Venenatis Condimentum Sollicitudin</span>
+							<p class="excerpt">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Quam Ultricies Mattis</h3>
+							<span class="dept">Venenatis Condimentum Sollicitudin</span>
+							<p class="excerpt">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Quam Ultricies Mattis</h3>
+							<span class="dept">Venenatis Condimentum Sollicitudin</span>
+							<p class="excerpt">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Quam Ultricies Mattis</h3>
+							<span class="dept">Venenatis Condimentum Sollicitudin</span>
+							<p class="excerpt">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Quam Ultricies Mattis</h3>
+							<span class="dept">Venenatis Condimentum Sollicitudin</span>
+							<p class="excerpt">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Quam Ultricies Mattis</h3>
+							<span class="dept">Venenatis Condimentum Sollicitudin</span>
+							<p class="excerpt">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+						</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" class="sidebar_article">
+							<img class="thumb" src="images/110x110.jpg">
+							<h3 class="title">Quam Ultricies Mattis</h3>
+							<span class="dept">Venenatis Condimentum Sollicitudin</span>
+							<p class="excerpt">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+						</a>
+					</li>
+					<hr>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="js/jquery-ui-1.8.22.custom.min.js"></script>
+    <script src="js/jquery.imagesloaded.min.js"></script>
+    <script src="js/video.js"></script>
+
+    <!-- BigVideo -->
+    <script src="js/bigvideo.js"></script>
+
+    <!-- Demo -->
+    <script>
+	    $(function() {
+		    var BV = new $.BigVideo();
+		    BV.init();
+		    BV.show('vids/000016662489_HDFlashVideo.mp4');
+		});
+    </script>
+</body>
+</html>
--- stretch.php
+++ stretch.php
@@ -15,11 +15,11 @@
 	<div class="clearfix">
 		<div class="column-left">
 			<div class="column-left-interior">
-				span left
+				<img src="images/second.jpg">
 			</div>
 			<div class="inner-nav">
 				<div class="inner-nav-intro">
-					inner nav
+					
 				</div>
 				<ul class="unstyled story-nav">
 					<li>
@@ -35,20 +35,8 @@
 						</a>
 					</li>
 					<li>
-						<a href="#">
+						<a href="#article-3">
 							<img src="images/story-nav-3.jpg" alt="">
-							<span>Look Once, Think Twice</span>
-						</a>
-					</li>
-					<li>
-						<a href="#">
-							<img src="images/story-nav-4.jpg" alt="">
-							<span>Look Once, Think Twice</span>
-						</a>
-					</li>
-					<li>
-						<a href="#">
-							<img src="images/story-nav-5.jpg" alt="">
 							<span>Look Once, Think Twice</span>
 						</a>
 					</li>
@@ -56,12 +44,13 @@
 			</div>
 		</div>
 		<div class="column-right">
+
 			<article id="article-1" class="article">
 				<div class="article-img">
-					<img src="images/future.jpg" alt="">
+					<img src="images/excel-finkbine.jpg" alt="">
 				</div>
 				<div class="article-body">
-					<h1>Look Once, Think Twice</h1>
+					<h1>The Finkbine Tradition Continues</h1>
 					<div class="article-intro">
 						<p>Student Health &amp; Wellness launched their new class “Look Once, Think Twice” in Fall 2012 and is specifically designed for students who are in the presence of alcohol, but not consuming alcohol.</p>
 					</div>
@@ -69,36 +58,31 @@
 						<p>WORDS BY <em id="page-article-author">Caitlin Scott</em></p>
 						<p>PHOTO BY <em>Ben Lewis</em></p>
 					</div>
-					<div class="article-share">
-
-					</div>
 					<div class="article-content">
-						<p>“[Look Once, Think Twice] lets students know they’re not the only person who has made the choice of abstinence,” says Danielle McGraw, Substance Abuse Counselor at UI. “It helps to kind of tie them to other people and to other resources. It also helps to increase students’ confidence.”</p>
-						<p>According to McGraw, a potentially high-risk situation Student Health staff often see occurs when a student invites friends over to their residence hall room to drink. The other roommate might not be comfortable with the situation, but they may be unsure of how to handle the situation.</p>
-						<p>Students living in the residence halls who are found to be in the presence of alcohol but not drinking, such as a roommate or friend, will face disciplinary action along with those consuming alcohol. These bystanders can either pay a fine ranging from $200 – $700, or choose to participate in the Look Once, Think Twice program. Prior to Fall 2012, all students would take the alcohol education workshop eCHECKUP TO GO before coming to campus. Student Health & Wellness staff felt that the workshop was not resonating with program participants.</p>
-						<p>“We felt that a lot of those students might not be consuming alcohol.” says McGraw. “Many of the students might have already made the choice to be abstinent, so [eCHECKUP TO GO] was missing key messages for them.”</p>
-						<p>The Look Once, Think Twice program seeks to meet the needs of students who choose abstinence as well as students who choose to consume alcohol in a low-risk manner.</p>
-						<h2>Bystander Intervention Techniques</h2>
-						<p>Look Once, Think Twice has three main objectives. The first goal is to ensure that students are able to identify bystander intervention techniques, which are used widely across the country, according to McGraw. The idea is to train bystanders to take actions other than silence or inactivity. For example, a first-year student living in the residence halls might confront a roommate who decides to drink alcohol in the dorm, as opposed to keeping quiet about the situation.</p>
-						<p>“They learn how to handle situations that they might feel uncomfortable with and how to make better choices when a situation arises,” says McGraw. “It’s refreshing for them to know that they are not the only person that doesn’t know what to what to do or how to say something,”</p>
-						<div class="full-quote"><p>“It’s refreshing for them to know that they are not the only person that doesn’t know what to what to do or how to say something.”</p></div>
-						<p>Out of the 30 students that took the Look Once, Think Twice post-assessment survey, 29 students were able to identify at least one bystander intervention technique. Additionally, students are 22% more likely to seek out a neutral third party when in a high-risk drinking situation, according to the survey.</p>
-						<h2>Locate alcohol-free resources at UI</h2>
-						<p>Look Once, Think Twice also aims to inform students of the alcohol-free resources available on campus. The objective is to ensure that students can name and locate at least one university resource for alcohol-free events. According to the post-assessment survey, 100% of the students were able to identify and locate a website for alcohol-free events, or a person that would know, such as an advisor or an Residence Hall Assistant.</p>
-						<h2>Confidence</h2>
-						<p>The final objective of Look Once, Think Twice, is to boost students’ confidence in their ability to address a situation when it does not align with their values. Participating students go through a list of values and they are asked to identify which values they align with. According to McGraw, this helps students to understand their actions, which likely tie into one of their core values.</p>
-						<div class="full-image">
-							<img src="images/full-open.jpg" alt="">
-						</div>
-						<p>“Sitting down and looking at values is something that a lot of students don’t really have the chance to do and I think it’s important to be able to do that,” says McGraw.</p>
-						<p>Before taking the class, students reported an average confidence level in their ability to address situations that don’t align with their values at 3.04 out of 4 on a Likert scale. The Likert scale is a psychometric scale commonly used  in research that uses questionnaires. The scale is one of the most common methods of assessing responses in survey research. After taking Look Once, Think Twice, students reported a confidence level of 3.30, which constitutes a 26% increase in confidence.</p>
-						<p>“We are not in the business of preaching abstinence to people. We just want to help students reduce any type of negative effects form their own drinking, and from someone else’s drinking,” says McGraw. “We want to do anything we can to increase success of students here at the university and we think that bystander techniques and harm reduction efforts really play a big role in these efforts.”</p>
+						<p>The Hancher-Finkbine Medallion is one of Iowa’s most prestigious awards. Receiving the award is an honor and instills pride in its recipients. As a recent graduate and the former UISG president for the 2012-2013 academic year, Nic Pottebaum’s hard work as a campus leader was recognized at the annual Finkbine Dinner.</p>
+
+						<p>“Somewhere along the way I became a campus leader, but I never thought of it as striving for that title,” says Pottebaum. “Rather, I saw the opportunities that made me a campus leader just an opportunity to benefit my fellow students.”</p>
+
+						<p>Pottebaum was one of seven award recipients recognized for their work on campus. This annual tradition of recognizing campus leaders had simple beginnings an event to bring people together on campus. Starting in 1917, dinners sponsored by William Finkbine and Carl Kuhnle were meant to gather leaders on campus, both students and professionals, to create connections and lasting ties on campus. The dinners were so successful that Finkbine created an endowment for them to continue.</p>
+
+						<p>Each year a total of seven students, faculty, staff, and alumni are invited to attend the Finkbine dinner along with the UI president, deans, and other members of the UI. It was Finkbine’s hope that campus leaders would come together to have a better understanding of each other and their shared interest in the traditions and livelihood of the university. Four students, one professor, one staff member, and one alumnus are given awards each year. </p>
+
+						<div class="full-quote"><p>“Every time I just get amazed [by the students] because they do something different that I have just never heard of before.”</p></div>
+
+						<p>“Every time I just get amazed [by the students] because they do something different that I have just never heard of before,” says Belinda Marner, Assistant Vice President for Student Life and Chair of the Finkbine Dinner Committee. “It’s one of the highlights of the year for me because you just think about all the things these people are doing and where they’re going - it’s amazing to think about.”</p>
+
+						<p>Award recipients are honored with medallions to recognize leadership, learning, and loyalty. The tradition of awarding medallions was established in 1964. These medallions are named after William Finkbine as well as Virgil Hancher who was a guest at the first dinner and served as president of the UI for 24 years.</p> 
+
+						<p>Pottebaum believes the process of honoring positive qualities is important at the university. The awards encourage staff, students, faculty, and alumni to be good leaders and community members in the Finkbine tradition.</p> 
+
+						<p>“My experience at Iowa taught me to balance many things while always valuing those around and doing things the right way,” says Pottebaum. “There are countless failures and successes I learned in my four years that will guide me for the rest of my life.”</p>
 					</div>
 				</div>
 			</article>
+
 			<article id="article-2" class="article">
 				<div class="article-img">
-					<img src="images/future.jpg" alt="">
+					<img src="images/future.jpg" alt="" height="400">
 				</div>
 				<div class="article-body">
 					<h1>Look Once, Think Twice</h1>
@@ -128,13 +112,65 @@
 						<h2>Confidence</h2>
 						<p>The final objective of Look Once, Think Twice, is to boost students’ confidence in their ability to address a situation when it does not align with their values. Participating students go through a list of values and they are asked to identify which values they align with. According to McGraw, this helps students to understand their actions, which likely tie into one of their core values.</p>
 						<div class="full-image">
-							<img src="images/full-open.jpg" alt="">
+							<img src="images/full-open.jpg" alt="" height="300">
 						</div>
 						<p>“Sitting down and looking at values is something that a lot of students don’t really have the chance to do and I think it’s important to be able to do that,” says McGraw.</p>
 						<p>Before taking the class, students reported an average confidence level in their ability to address situations that don’t align with their values at 3.04 out of 4 on a Likert scale. The Likert scale is a psychometric scale commonly used  in research that uses questionnaires. The scale is one of the most common methods of assessing responses in survey research. After taking Look Once, Think Twice, students reported a confidence level of 3.30, which constitutes a 26% increase in confidence.</p>
 						<p>“We are not in the business of preaching abstinence to people. We just want to help students reduce any type of negative effects form their own drinking, and from someone else’s drinking,” says McGraw. “We want to do anything we can to increase success of students here at the university and we think that bystander techniques and harm reduction efforts really play a big role in these efforts.”</p>
 					</div>
+				</div><!-- end .article-body -->
+			</article>
+			
+			<article id="article-3" class="article">
+				<div class="article-img">
+					<img src="images/excel-outdoor-conf.jpg" alt="">
 				</div>
+				<div class="article-body">
+					<h1>Midwest Outdoor Leadership Conference</h1>
+					<div class="article-intro">
+						<p>Student Health &amp; Wellness launched their new class “Look Once, Think Twice” in Fall 2012 and is specifically designed for students who are in the presence of alcohol, but not consuming alcohol.</p>
+					</div>
+					<div class="article-meta">
+						<p>WORDS BY <em id="page-article-author">Caitlin Scott</em></p>
+						<p>PHOTO BY <em>Ben Lewis</em></p>
+					</div>
+					<div class="article-content">
+						<p>The University of Iowa hosted nearly 100 students at the second annual Midwest Outdoor Leadership Conference (MWOLC) on Feb. 22-24, 2013. Every year MWOLC invites students from college outdoor recreation programs across the Midwest to meet for one weekend in order to learn skills that will benefit their future careers in the outdoor recreation industry.</p>
+
+						<p>According to David Patton, Assistant Director for Recreational Services and Outdoor Programs at UI, the MWOLC is modeled on the Association of Outdoor Recreation and Education (AORE) Conference.</p>
+
+						<p>“I go to AORE every year, but it’s mainly people like me, the full-time person that runs the outdoor program, and it’s sort of limiting to students,” Patton says, referencing AORE’s high cost, week-long timeframe, and distant location.  “It’s great for [full-time staff] to do that and stay on the edge, but we need opportunities for the students.”</p>
+
+						<p>Outdoor recreation professionals at Miami University in Oxford, Ohio pinpointed the growing need for a convention tailored to aspiring outdoor recreation leaders and set out to create the first-ever Midwest Outdoor Leadership Conference, which Ohio State University hosted in 2011.</p>
+
+						<p>“Last year, I promised that I’d bring a van full of Iowa students out to Ohio State to support the conference and we did,” Patton says. “We had so much fun. The conference organizers saw our enthusiasm and they asked us to host it this year, so we did.”</p>
+
+						<p>MWOLC featured breakout sessions with special emphasis on leadership development, program management, professional development, and skill development. Attendees also had the chance to relax and have fun with events like a bouldering competition, open climbing time, and a documentary viewing.</p>
+
+						<div class="full-quote"><p>“We had so much fun. The conference organizers saw our enthusiasm and they asked us to host it this year, so we did.”</p></div>
+
+						<p>In order to keep the conference accessible to all students, the MWOLC charged a conference registration fee of $50, a relatively small payment considering that professional conferences like AORE cost hundreds of dollars. Students were able to camp on the turf of the Hawkeye Tennis and Recreation Complex (HTRC) for free. Marmot outfitters sponsored a raffle and the keynote speaker, professional mountaineer Fabrizio Zangrilli. The well-known mountaineer gave students an in-depth look at what it’s like to be a leader in outdoor recreation. Zangrilli told students about his extensive list of outdoor experiences, including key accomplishments like climbing K2 and guiding groups of people through the Himalayas.</p>
+
+						<p>Conference attendees enjoyed the opportunity to learn from seasoned outdoor leaders like Zangrilli, and were grateful for the ability to network with like-minded people.</p>
+
+						<p>“It is so easy to meet and talk to new people because we’re all here for the same thing. It’s been really helpful,” says Courtney Sullivan, junior at Bowling Greens State University.</p>
+
+						<p>Patton’s entire student staff at the Campus Recreation and Wellness Center participated in the conference. The students were able to waive  the registration fee in exchange for volunteering to work the conference.
+						Patton was proud to host the 2013 conference at UI’s facilities, but his favorite part of the three-day weekend was sharing the world of outdoor recreation with students.</p>
+
+						<p>“The highlight of the conference for me is seeing the younger folks. It opens their eyes a bit to how big outdoor recreation is. It’s not just students at the Iowa Rec Center working the climbing wall,” Patton says. “It’s great for my staff to see that there are other people in the world, and other universities that do the same sort of thing that we do.”</p>
+
+						<p>Students that attended the conference are just as pleased with the event as Patton. Andrew McQuillen, a student at the University of Dayton, shared his comments on the value of the weekend to Facebook.</p>
+
+						“The Midwest Outdoor Leadership Conference at the University of Iowa was amazing. I heard some incredible stories and met some incredible people, all who share an awesome passion for the outdoors. I’m 100% confident in my decision to pursue outdoor education and recreation as a career.”</p>
+
+						<div class="full-image">
+							<img src="images/excel-outdoorconf-int1.jpg" alt="">
+						</div>
+
+						<p>MWOLC Keynote Speaker Zangrilli also gave UI conference organizers positive feedback. Zangrilli published his thoughts to Facebook, saying  “I had a super fun time, met some really interesting and motivated people and got to sample some great nightlife in Iowa City as well.” The mountaineer also playfully added a note to the school who hosts next year’s conference.  “Good luck next year to the university that puts the event on – [a] tough act to follow for sure!”</p>
+					</div>
+				</div><!-- end .article-body -->
 			</article>
 		</div>
 	</div>
